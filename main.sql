8bc28a8cb2678375626f66d9e435e1822e246d61
B1GM0NE3 <slavasemenov6037@gmail.com>
2026-07-21 19:59:03 +0300

    Lesson 03 - attempt 3

--- main.sql
+++ main.sql
@@ -1,1 +1,13 @@
-select name from cities 
+create table cities (
+    id interger primary key,
+    name text not null,
+    country text not null,
+    population interger
+);
+
+insert into cities (name, country, population) values 
+    ('Tokyo',  'JP', 13960000),
+    ('Osaka',  'JP', 2691000),
+    ('Berlin', 'DE', 3645000);
+
+select "name" from cities
